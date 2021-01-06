ccf1d5b03ef25b9bc3b2a6af2a165f86c59f425b
Vargyas-Moni Lajos <lajos@octopus-digital.pro>
2021-01-06 18:11:53 +0200

    created learning project

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController:ApiController
+    {
+        public IHttpActionResult Get()
+        {
+          
+            return Ok(new {Name = "Lajos", Occupation = "Developer"});
+        }
+    }
+}
